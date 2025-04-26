f272e3e45061df76dff8404655226ca442429ab1
Julian Andres Ramirez <116443557+jramirez0982@users.noreply.github.com>
2025-04-26 03:07:40 +0000

    [creacion de cartas aleatorias] index.html: Cree la tarjeta app.js: Creo codigo para generar cartas aleatorias style.css: defino estilos de nuevas clases

--- src/app.js
+++ src/app.js
@@ -5,7 +5,38 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+function getRandomElement(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let arrayPalos = ["♦", "♥", "♠", "♣"];
+  let arrayNumbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+  let paloSelected = getRandomElement(arrayPalos);
+  let numberSelected = getRandomElement(arrayNumbers);
+  let classSelected = "";
+
+  if (paloSelected == "♦" || paloSelected == "♥") {
+    classSelected = "heart-diamond";
+  }
+  else {
+    classSelected = "spades-cube";
+  }
+
+  let paloSup = document.getElementById("palo-superior");
+  paloSup.classList.add(classSelected);
+  paloSup.innerText = paloSelected;
+
+  let paloInf = document.getElementById("palo-inferior");
+  paloInf.classList.add(classSelected);
+  paloInf.innerText = paloSelected
+
+  document.getElementById("numero").innerText = numberSelected;
+  
+  //console.log("Hello Rigo from the console!");
+
+  
 };
